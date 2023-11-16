96755b29a1a3d8efc6f55b3aaba9167dba24298e
Philip Gyamfi <philab.pg@gmail.com>
2023-11-16 10:38:59 +0000

    pp

--- src/ussd/ussd.service.ts
+++ src/ussd/ussd.service.ts
@@ -24,53 +24,67 @@ export class UssdService {
 
     if(text === ""){
       //this is the first request. Note how we start the response with CON
-      response = `CON Welcome to PeoplesPay ?
+      return response = `CON Welcome to PeoplesPay ?
       1. Check Balance
       2. Make Payment
       3. Accept Payment
       4. Make a Donation
       `;
-    } else if(text === "1"){
-      //Business logic for first level response 
-      response = `CON Enter The Merchant Code`;
-    } else if(text === "2"){
-      //Get the mobile number from firestore Database
-  
-      //Terminal Request
-      response = `CON Enter The Merchant Code`
-    } else if(text === "3"){
-      response = `CON Enter Your Merchant Code`;
-  
-    } else if(text === "4"){
-      response = `CON Enter Recipient Code`;
-    } else if(text === `1*${merchantCode}`){
-      const balance = "ghc2000";
-      response = `END Your Balance Is ${balance}`
-    } else if(text === `2*${merchantCode}`){
-      response = `CON Enter Amount to Pay`;
-
-     // amount_pay = text.split("*")[2];
-  
-    } else if(text === `3*${merchantCode}`){
-      response = `CON Enter Amount to pay`;
-      const amount = text.split("*")[2];
-      response = `CON Enter Payee Wallet Number`;
-      const walletNumber = text.split("*")[3];
-      //API Goes Here
-      response = `END Payee Wallet Number Is ${walletNumber}`
-
-    }else if(text === `4*${recipientCode}`){
-      response = `CON Enter Donation Amount`
-      const damount = text.split("*")[2];
-      //API Goes Here
-      response = `END Donation Amount Is ${damount}`
-    }else if(text){
-      // response = `CON Enter Amount to Pay`;
-      // const amount = text.split("*")[2];
-      //Api Goes Here
-      //const amt = text.split("*");
-      response = `END Your Amount Entered Is ${text}`
     }
+
+    if(text === "1" || text === "2" || text === "3" || text === "4"){
+      if(text === "4")
+      {
+        return response = `CON Enter Recipient Code`;
+      }
+      return response = `CON Enter The Merchant Code`;
+    } else {
+      return response = `END Select A Valid Option`; 
+    }
+
+
+
+    // } else if(text === "1"){
+    //   //Business logic for first level response 
+    //   response = `CON Enter The Merchant Code`;
+    // } else if(text === "2"){
+    //   //Get the mobile number from firestore Database
+  
+    //   //Terminal Request
+    //   response = `CON Enter The Merchant Code`
+    // } else if(text === "3"){
+    //   response = `CON Enter Your Merchant Code`;
+  
+    // } else if(text === "4"){
+    //   response = `CON Enter Recipient Code`;
+    // } else if(text === `1*${merchantCode}`){
+    //   const balance = "ghc2000";
+    //   response = `END Your Balance Is ${balance}`
+    // } else if(text === `2*${merchantCode}`){
+    //   response = `CON Enter Amount to Pay`;
+
+    //  // amount_pay = text.split("*")[2];
+  
+    // } else if(text === `3*${merchantCode}`){
+    //   response = `CON Enter Amount to pay`;
+    //   const amount = text.split("*")[2];
+    //   response = `CON Enter Payee Wallet Number`;
+    //   const walletNumber = text.split("*")[3];
+    //   //API Goes Here
+    //   response = `END Payee Wallet Number Is ${walletNumber}`
+
+    // }else if(text === `4*${recipientCode}`){
+    //   response = `CON Enter Donation Amount`
+    //   const damount = text.split("*")[2];
+    //   //API Goes Here
+    //   response = `END Donation Amount Is ${damount}`
+    // }else if(text){
+    //   // response = `CON Enter Amount to Pay`;
+    //   // const amount = text.split("*")[2];
+    //   //Api Goes Here
+    //   //const amt = text.split("*");
+    //   response = `END Your Amount Entered Is ${text}`
+    // }
 
     return response
   }
